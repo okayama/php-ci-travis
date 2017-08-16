a80e6c4dd7db02204ebfa1fe375733af7c6defb5
okayama <kondoh@alfasado.jp>
2017-08-16 14:59:13 +0900

    add unit tests and settings for travis

---
+++ tests/sampleTest.php
@@ -0,0 +1,13 @@
+<?php
+class TestSample extends PHPUnit_Framework_TestCase
+{
+  function testSample1()
+  {
+    $this->assertEquals(2, 1 + 1);
+  }
+  function testSample2()
+  {
+    $this->assertEquals(2, 1 - 1);
+  }
+}
+?>
